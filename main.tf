5a9359f1b644098ce703d2df7afdf6b614b954b4
Sam Crudge <samuel.crudge@cyber-duck.co.uk>
2022-02-16 22:59:26 +0000

    pipeline files renamed, added required module files, data calls added,  outputfiles.

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    buddy = {
+      source  = "buddy/buddy"
+      version = "1.3.2"
+    }
+  }
+}
+
+provider "buddy" {
+  # Configuration options
+  token = var.buddy_token
+
+  base_url = var.base_url
+  insecure = var.insecure
+}
+
+
+
+
+resource "buddy_pipeline" "test" {
+  count = var.pipeline ? 1 : 0
+
+  domain                        = var.domain
+  project_name                  = data.buddy_project.display_name
+  name                          = var.pipe_name
+  on                            = var.pipeline_react
+  refs                          = var.pipeline_react == "CLICK" ? var.pipeline_ref : null
+  always_from_scratch           = var.pipeline_always_from_scratch
+  auto_clear_cache              = var.pipeline_auto_clear_cache
+  cron                          = var.pipeline_start_date != null && var.pipeline_delay != null ? null : var.pipeline_cron
+  delay                         = var.pipeline_start_date != null ? null : var.pipeline_cron
+  do_not_create_commit_status   = var.pipeline_commit_status
+  event                         = var.pipeline_react == "EVENT" ? var.pipeline_event : null
+  execution_message_template    = var.pipeline_execution_msg_template
+  fail_on_prepare_env_warning   = var.pipeline_env_warning_fail
+  fetch_all_refs                = var.pipeline_fetch_all_refs
+  ignore_fail_on_project_status = var.pipeline_ignore_fail_status
+  no_skip_to_most_recent        = var.pipeline_no_skip_most_recent
+  paused                        = var.pipeline_react == "SCHEDULE" ? var.pipeline_paused : null
+  priority                      = var.pipeline_priority
+  start_date                    = var.pipeline_react == "SCHEDULE" && var.cron == null ? var.pipeline_start_date : null
+  target_site_url               = var.pipeline_target_url
+  trigger_condition             = var.pipeline_trigger_condition
+  worker                        = var.pipeline_worker
+}
